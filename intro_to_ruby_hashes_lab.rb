6bcac45d8f4322b92c213fd70cb1ccf12fabb3b4
Lana Worobec <llama.p0rtk3y@gmail.com>
2019-02-23 22:06:30 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,16 +3,16 @@ def new_hash
 end
 
 def actor
-  actor = {:name=>"Dwayne The Rock Johnson"}
+  actor = {:name =>"Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {:railroads=>{}}
+	monopoly = {:railroads =>{}}
 
 end
 
 def monopoly_with_second_tier
-	monopoly = {:railroads=>{:pieces=>4, :rent_in_dollars=>{}, names=>{}}
+	monopoly = {:railroads =>{:pieces =>4, :rent_in_dollars =>{}, names =>{}}
 
 end
 
